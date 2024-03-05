2ced2975974cc85caff3ee5922c5339f8329aea2
fturannn <furkn.turan@hotmail.com>
2024-03-05 15:38:37 +0300

    Update User & UserReview Controllers

--- user-service/src/main/java/com/n11/userservice/controller/UserController.java
+++ user-service/src/main/java/com/n11/userservice/controller/UserController.java
@@ -1,4 +1,50 @@
 package com.n11.userservice.controller;
 
+import com.n11.userservice.controller.contract.UserControllerContract;
+import com.n11.userservice.dto.UserDTO;
+import com.n11.userservice.general.RestResponse;
+import com.n11.userservice.request.UserSaveRequest;
+import com.n11.userservice.request.UserUpdateRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/users")
 public class UserController {
+
+    private final UserControllerContract userControllerContract;
+
+    @GetMapping
+    public ResponseEntity<RestResponse<List<UserDTO>>> getAllUsers() {
+        List<UserDTO> userDTOS = userControllerContract.getAll();
+        return ResponseEntity.ok(RestResponse.of(userDTOS));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RestResponse<UserDTO>> getUserById(@PathVariable Long id) {
+        UserDTO userDTO = userControllerContract.getById(id);
+        return ResponseEntity.ok(RestResponse.of(userDTO));
+    }
+
+    @PostMapping
+    public ResponseEntity<RestResponse<UserDTO>> saveUser(@RequestBody UserSaveRequest request) {
+        UserDTO userDTO = userControllerContract.save(request);
+        return ResponseEntity.ok(RestResponse.of(userDTO));
+    }
+
+    @PutMapping("/{debugUserId}")
+    public ResponseEntity<RestResponse<UserDTO>> updateUser(@PathVariable Long debugUserId,
+                                                        @RequestBody UserUpdateRequest request) {
+        UserDTO userDTO = userControllerContract.update(debugUserId, request);
+        return ResponseEntity.ok(RestResponse.of(userDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userControllerContract.delete(id);
+    }
 }
--- user-service/src/main/java/com/n11/userservice/controller/UserReviewController.java
+++ user-service/src/main/java/com/n11/userservice/controller/UserReviewController.java
@@ -1,4 +1,50 @@
 package com.n11.userservice.controller;
 
+import com.n11.userservice.controller.contract.UserReviewControllerContract;
+import com.n11.userservice.dto.UserReviewDTO;
+import com.n11.userservice.general.RestResponse;
+import com.n11.userservice.request.UserReviewSaveRequest;
+import com.n11.userservice.request.UserReviewUpdateRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/reviews")
 public class UserReviewController {
+
+    private final UserReviewControllerContract userReviewControllerContract;
+
+    @GetMapping
+    public ResponseEntity<RestResponse<List<UserReviewDTO>>> getAllReviews() {
+        List<UserReviewDTO> userReviewDTOS = userReviewControllerContract.getAll();
+        return ResponseEntity.ok(RestResponse.of(userReviewDTOS));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RestResponse<UserReviewDTO>> getReviewById(@PathVariable Long id) {
+        UserReviewDTO userReviewDTO = userReviewControllerContract.getById(id);
+        return ResponseEntity.ok(RestResponse.of(userReviewDTO));
+    }
+
+    @PostMapping
+    public ResponseEntity<RestResponse<UserReviewDTO>> saveReview(@RequestBody UserReviewSaveRequest request) {
+        UserReviewDTO userReviewDTO = userReviewControllerContract.save(request);
+        return ResponseEntity.ok(RestResponse.of(userReviewDTO));
+    }
+
+    @PutMapping("/{debugReviewId}")
+    public ResponseEntity<RestResponse<UserReviewDTO>> updateReview(@PathVariable Long debugReviewId,
+                                                                    @RequestBody UserReviewUpdateRequest request) {
+        UserReviewDTO userReviewDTO = userReviewControllerContract.update(debugReviewId, request);
+        return ResponseEntity.ok(RestResponse.of(userReviewDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteReview(@PathVariable Long id) {
+        userReviewControllerContract.delete(id);
+    }
 }
